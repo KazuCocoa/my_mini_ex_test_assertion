9b8e08c252891fb8b2613b5f4eb929e4015bb036
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-08-18 14:59:42 +0900

    add test macro but @tests are empty

--- assertion.exs
+++ assertion.exs
@@ -9,9 +9,21 @@ defmodule Assertion do
     quote do
       import unquote __MODULE__
 
+      # http://elixir-lang.org/docs/stable/elixir/Module.html#register_attribute/3
+      Module.register_attribute __MODULE__, :tests, accumulate: true
+
       def run do
-        IO.puts "Running the tests..."
+        IO.puts "Running the tests (#{inspect @tests})"
       end
+    end
+  end
+
+  defmacro test(description, do: test_block) do
+    test_func = String.to_atom(description)
+
+    quote do
+      @tests {unquote(test_func), unquote(description)}
+      def unquote(test_func)(), do: unquote(test_block)
     end
   end
 end
@@ -48,11 +60,11 @@ end
 defmodule MathTest do
   use Assertion
 
-#  test "integraters can be added and subtraced" do
-#    assert 1 + 1 == 2
-#    assert 2 + 3 == 5
-#    assert 5 - 5 == 10
-#  end
+  test "integraters can be added and subtraced" do
+    assert 1 + 1 == 2
+    assert 2 + 3 == 5
+    assert 5 - 5 == 10
+  end
 #
 #  test "integraters can be multiplied and divided" do
 #    assert 5 * 5 == 25
