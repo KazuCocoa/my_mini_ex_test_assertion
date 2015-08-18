f93601f41b9d53dceaf5a445d679d9262dc823dc
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-08-18 16:49:00 +0900

    add new run to invoke all tests

--- assertion.exs
+++ assertion.exs
@@ -1,10 +1,4 @@
 defmodule Assertion do
-  defmacro assert({operator, _, [lhs, rhs]}) do
-    quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
-      Assertion.Test.assert operator, lhs, rhs
-    end
-  end
-
   defmacro __using__(_options) do
     quote do
       import unquote(__MODULE__)
@@ -20,43 +14,63 @@ defmodule Assertion do
 
   defmacro __before_compile__(_env) do
     quote do
-      def run do
-        IO.puts "Running the tests (#{inspect @tests})"
-      end
+      def run, do: Assertion.Test.run(@tests, __MODULE__)
     end
   end
 
   defmacro test(description, do: test_block) do
     test_func = String.to_atom(description)
-
     quote do
       @tests {unquote(test_func), unquote(description)}
       def unquote(test_func)(), do: unquote(test_block)
     end
   end
+
+  defmacro assert({operator, _, [lhs, rhs]}) do
+    quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
+      Assertion.Test.assert operator, lhs, rhs
+    end
+  end
 end
 
 defmodule Assertion.Test do
+  def run(tests, module) do
+    Enum.each tests, fn {test_func, description} ->
+      case apply(module, test_func, []) do
+        :ok             -> IO.write "."
+        {:fail, reason} -> IO.puts """
+
+          ==================================
+          FAILURE: #{description}
+          ==================================
+          #{reason}
+          """
+      end
+    end
+  end
+
   def assert(:==, lhs, rhs) when lhs == rhs do
-    IO.write "."
+    :ok
   end
   def assert(:==, lhs,  rhs) do
-    IO.puts """
-    FAILURE:
-      Expected:       #{lhs}
-      to be equal to: #{rhs}
-    """
+    {:fail, """
+      FAILURE:
+        Expected:       #{lhs}
+        to be equal to: #{rhs}
+      """
+    }
   end
 
   def assert(:>, lhs, rhs) when lhs > rhs do
-    IO.write "."
+    :ok
   end
   def assert(:>, lhs, rhs) do
-    IO.puts """
-    FAILURE:
-      Expected:       #{lhs}
-      to be equal to: #{rhs}
-    """
+    {:fail, """
+      FAILURE:
+        Expected:       #{lhs}
+        to be equal to: #{rhs}
+      """
+    }
   end
 end
 
@@ -72,9 +86,9 @@ defmodule MathTest do
     assert 2 + 3 == 5
     assert 5 - 5 == 10
   end
-#
-#  test "integraters can be multiplied and divided" do
-#    assert 5 * 5 == 25
-#    assert 10 / 2 == 5
-#  end
+
+  test "integraters can be multiplied and divided" do
+    assert 5 * 5 == 25
+    assert 10 / 2 == 5
+  end
 end
